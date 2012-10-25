a7afd8abb0d8b5cf6ebb5d94ec6c033c0800e74d
byels <byelsey1@gmail.com>
2012-10-25 00:50:20 -0400

    @%$%$!& oops

---
+++ src/doll/views/ajax.clj
@@ -0,0 +1,12 @@
+(ns doll.views.ajax
+  (:use [noir.core :only [defpage]])
+  (:require [noir.session :as session]
+            [noir.cookies :as cookies]
+            [noir.response :as response]))
+
+(defpage [:get ["/character/:id" :id #"[\w]+"]] {:keys [id]}
+         (response/json (session/get :client)))
+
+(defpage [:post ["/character/:id" :id #"[\w]+"]] {:keys [id x y z yrot]}
+         (session/put! :client {:id id :x x :y y :z z :yrot yrot})
+         (session/get :client))
